007f7c1b10aa715852df110e5774ee4fae835f38
tollyx <adrian@tollyx.net>
2016-01-12 20:13:29 +0100

    Collisionmanager stuff

--- RougeLike/CollisionManager.h
+++ RougeLike/CollisionManager.h
@@ -5,8 +5,9 @@ class Collider;
 class CollisionManager
 {
 public:
-	static bool Check(Collider* p_pxShapeA, Collider* p_pxShapeB, 
-		int& p_iOverlapX, int& p_iOverlapY);
+	static bool AABB(Collider* p_pxShapeA, Collider* p_pxShapeB);
+	static bool AABB(Collider* p_pxShapeA, Collider* p_pxShapeB, 
+		float& p_iOverlapX, float& p_iOverlapY);
 private:
 
 };
